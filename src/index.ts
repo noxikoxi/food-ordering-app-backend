c3b173a854f1a177b54062db09b5a7ce18d31527
noxikoxi <noxikoxi@gmail.com>
2024-03-29 23:34:35 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, {Request, Response} from "express"
+import cors from "cors"
+import "dotenv/config"
+import * as mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoutes"
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => {console.log("Connected to database!")})
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({message: "health OK!"});
+})
+
+app.use("/api/my/user", MyUserRoute);
+
+app.listen(7000, () => {
+    console.log("Server started on localhost:7000");
+})
